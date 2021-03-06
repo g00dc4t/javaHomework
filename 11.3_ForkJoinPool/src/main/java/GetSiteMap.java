2d5506563b0b189cf9e6b9a1845cb25fa34fe265
goodcat <evgeniy6belousov@gmail.com>
2021-03-06 16:23:33 +0300

    done with 11.3

--- 11.3_ForkJoinPool/src/main/java/GetSiteMap.java
+++ 11.3_ForkJoinPool/src/main/java/GetSiteMap.java
@@ -1,23 +1,13 @@
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.TreeSet;
 import java.util.concurrent.RecursiveTask;
 
 public class GetSiteMap extends RecursiveTask<List<String>> {
 
     private String siteUrl;
     private List<String> pathsList = new LinkedList<>();
-
 
     public GetSiteMap(String siteUrl) {
         this.siteUrl = siteUrl;
@@ -27,23 +17,12 @@ public class GetSiteMap extends RecursiveTask<List<String>> {
     protected List<String> compute() {
         List<GetSiteMap> tasks = new LinkedList<>();
         try {
-//            Elements urlsElements = Jsoup.connect(siteUrl).get().select("a");
-//            for (Element e : urlsElements) {
-//                String path = e.absUrl("href");
-//                if (path.contains(siteUrl) && !pathsList.contains(path)) {
-//                    pathsList.add(path);
-//                    GetSiteMap task = new GetSiteMap(path);
-//                    task.fork();
-//                    tasks.add(task);
-//                }
-//            }
-
             Jsoup.connect(siteUrl).get()
                     .select("a")
                     .forEach(element -> {
                         String path = element.absUrl("href");
                         if (path.contains(siteUrl) && !pathsList.contains(path)) {
-                            addRightFormat(path);
+                            addInRightFormat(path);
                             GetSiteMap task = new GetSiteMap(path);
                             task.fork();
                             tasks.add(task);
@@ -52,25 +31,11 @@ public class GetSiteMap extends RecursiveTask<List<String>> {
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        addResultFromTasks(pathsList, tasks);
         return pathsList;
     }
 
-    public void addResultFromTasks(List<String> pathsList, List<GetSiteMap> tasks) {
-        for (GetSiteMap task : tasks) {
-                pathsList.addAll(task.join());
-        }
-    }
-
-    public void addRightFormat(String path) {
+    public void addInRightFormat(String path) {
         String rightString = ("\n" + "\t".repeat(path.split("/").length - 3) + path);
         pathsList.add(rightString);
     }
-
-    public static void writeUrlInFile(String url, String fileName) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
-        writer.append("\n" + "\t".repeat(url.split("/").length - 3) + url);
-        writer.close();
-    }
-
 }
--- 11.3_ForkJoinPool/src/main/java/Main.java
+++ 11.3_ForkJoinPool/src/main/java/Main.java
@@ -1,16 +1,9 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 public class Main {
 
@@ -20,36 +13,14 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-//            createFile(FILE_NAME);
-//            ForkJoinPool forkJoinPool = new ForkJoinPool();
-//            GetSiteMap getSiteMap = new GetSiteMap(siteUrl);
-//            forkJoinPool.execute(getSiteMap);
+            createFile(FILE_NAME);
             siteMap = new ForkJoinPool().invoke(new GetSiteMap(siteUrl));
-
-
-//            do {
-//                TimeUnit.MILLISECONDS.sleep(100);
-//            } while (!getSiteMap.isDone());
-//            forkJoinPool.shutdown();
-//
-//            siteMap = getSiteMap.join();
-
             for (String s : siteMap) {
-                System.out.println(s);
+                writeInFile(s, FILE_NAME);
             }
-
-//            Comparator<String> comparator = Comparator.comparing(s -> s.split("/").length);
-//            comparator = comparator.thenComparing(Comparator.comparing(s -> s));
-//            getSiteMap.invoke().stream().sorted(comparator).forEach(System.out::println);
-//            siteMap = getSiteMap.invoke().stream().sorted(comparator).collect(Collectors.toList());
-
-//            for (String str : siteMap) {
-//                writeUrlInFile(str, FILE_NAME);
-//            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
     }
 
     public static void createFile(String fileName) throws IOException {
@@ -57,9 +28,9 @@ public class Main {
         Files.createFile(filePath);
     }
 
-    public static void writeUrlInFile(String url, String fileName) throws IOException {
+    public static void writeInFile(String url, String fileName) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
-        writer.append("\n" + "\t".repeat(url.split("/").length - 3) + url);
+        writer.append(url);
         writer.close();
     }
 }
