def6c99ac3de347ba54c49f70c56eabdd660a842
Технолог стан 320 <technolog320@bsw.iron>
2021-03-24 15:42:23 +0300

    done with 12 module

--- 12.ToDoList/src/main/java/main/Main.java
+++ 12.ToDoList/src/main/java/main/Main.java
@@ -2,6 +2,8 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class Main {
